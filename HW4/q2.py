f02d05e721d3217877660b8d4867198b8726149c
Anna <annarom99@gmail.com>
2019-01-17 23:45:58 +0200

    Finished q1

--- HW4/q2.py
+++ HW4/q2.py
@@ -5,8 +5,11 @@ df = pd.read_csv("DE_results_corrected.csv")
 df = df[['row', 'baseMean',	'log2FoldChange', 'lfcSE', 'stat', 'pvalue', 'padj']]
 #df = pd.read_csv("data\lung_counts.csv")
 
-df = df.dropna(subset=['padj'])
-df = df.sort_values(['padj'])
+#df = df.loc[(df['padj'] <= 0.05)]
+df = df.loc[(df['log2FoldChange'] > 2) | (df['log2FoldChange'] < -2)]
+
+#df = df.dropna(subset=['padj'])
+#df = df.sort_values(['padj'])
 
 df = df.reset_index()
 
@@ -14,8 +17,10 @@ find = re.compile(r"^[^.]*")
 
 for i in range(0, len(df.index)):
     df.at[i, 'row'] = re.search(find, df.iloc[i]['row']).group(0)
-   #df.at[i, 'ensgene'] = re.search(find, df.iloc[i]['ensgene']).group(0)
 
-df['row'].to_csv("ranked_genes.txt", index=False, header=False)
-# df['row'].to_csv("target_list.txt", index=False, header=False)
+print(df)
+
+#df['row'].to_csv("ranked_genes.txt", index=False, header=False)
+df['row'].to_csv("target_list_2.txt", index=False, header=False)
 #df['row'].to_csv("background_list_.txt", index=False, header=False)
+
