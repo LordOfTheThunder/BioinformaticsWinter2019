0b263d53f532013fd63fa47c07901deb6d3d16d5
Max Kolchinsky <max.kolchinsky@gmail.com>
2019-02-16 23:54:14 +0200

    Added accuracy calc on kmeans + hierarchical clustering

--- Project/code/clustering.py
+++ Project/code/clustering.py
@@ -4,6 +4,8 @@ from sklearn.metrics import silhouette_score
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from  matplotlib import colors
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering
 
 # ideas to remember: "help" the algorithm by filtering differentially expressed genes, create two features
 # which will be the avg of the expression data, one for treated and one for non-treated
@@ -16,8 +18,9 @@ samples=['GSM1338298', 'GSM1338302', 'GSM1338306', 'GSM1338297', 'GSM1338301', '
 # df = df.reset_index()
 # print(df)
 
-df = pd.read_csv("no_nan.csv")
+df = pd.read_csv("data_labeled_no_mean.csv")
 gene_exp = df[samples]
+filtered_gene_exp = df[df['apoptosis_related'] == True]
 
 #print(gene_exp)
 
@@ -25,13 +28,27 @@ gene_exp = df[samples]
 
 model = KMeans(n_clusters=5)
 clusters = model.fit_predict(gene_exp)
+predict_true = list(model.predict(filtered_gene_exp[samples]))
+count_list = [predict_true.count(i) for i in range(model.n_clusters)]
+accuracy = max(count_list) / len(predict_true)
+print("KMeans accuracy: ", accuracy)
 #silhouette_score = silhouette_score(gene_exp, clusters, metric='euclidean')
+# kmeans_2d = model.fit_transform(gene_exp)
+# color_map = colors.ListedColormap(['b', '#681E0E', 'g', '#7F7F7F', '#FF8700']) #, '#FF84E5', '#6600D0',
+#                                #    'r', 'c', '#D7D7D7', '#F4FF00'])
+# sc = plt.scatter(kmeans_2d[:,0],kmeans_2d[:,1], c=clusters, cmap=color_map)
+# cb = plt.colorbar(sc, ticks=range(5))
+# cb.set_label("Cluster number")
+# cb.set_ticks(range(5))
+# plt.show()
 
-
+# Hierarchical clustering - Need to finish this
+hier_cluster = AgglomerativeClustering(n_clusters=5)
+hier_clusters = hier_cluster.fit_predict(gene_exp)
 
 # plot clusters in 2D
 pca = PCA(n_components=2).fit(gene_exp)
-pca_2d = pca.transform(gene_exp)
+pca_2d = pca.fit_transform(gene_exp)
 color_map = colors.ListedColormap(['b', '#681E0E', 'g', '#7F7F7F', '#FF8700']) #, '#FF84E5', '#6600D0',
                                #    'r', 'c', '#D7D7D7', '#F4FF00'])
 sc = plt.scatter(pca_2d[:,0],pca_2d[:,1], c=clusters, cmap=color_map)
