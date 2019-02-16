d65ef578849be3d305e35b7cf2b0e144cc7c9dd9
Anna <annarom99@gmail.com>
2019-02-16 20:18:27 +0200

    Added train and test files

--- Project/code/classifying.py
+++ Project/code/classifying.py
@@ -15,13 +15,30 @@ samples=['GSM1338298', 'GSM1338302', 'GSM1338306', 'GSM1338297', 'GSM1338301', '
 
 if __name__ == "__main__":
     # warnings.filterwarnings("ignore")
+    #
+    # df = pd.read_csv("data_labeled.csv")
+    # data = df[df.columns[0:13]]
+    #
+    # print(data)
+    #
+    #
+    # labels = df['apoptosis_related']
+    #
+    # X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=.3, stratify=labels)
+    #
+    # X_train = X_train.assign(label = y_train)
+    # X_train.to_csv('train_probes.csv')
+    #
+    # X_test = X_test.assign(label = y_test)
+    # X_test.to_csv('test_probes.csv')
 
-    df = pd.read_csv("data_labeled.csv")
-    data = df[df.columns[1:12]]
-    labels = df['apoptosis_related']
+    train = pd.read_csv("train_mean.csv")
+    X_train = train[samples]
+    y_train = train['label']
 
-    # Limit to the two first classes, and split into training and test
-    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=.3, stratify=labels)
+    test = pd.read_csv("test_mean.csv")
+    X_test = test[samples]
+    y_test = test['label']
 
     # Create a simple classifier
     clf = DecisionTreeClassifier(class_weight='balanced')
@@ -30,7 +47,7 @@ if __name__ == "__main__":
 
     # Classify with SVM
     clf = svm.LinearSVC(class_weight='balanced')
-    svm = svm_classifier(data, labels, clf)
+    svm = svm_classifier(X_train, y_train, clf)
     y_pred_svm = clf.predict(X_test)
 
     # Classify with KNN
@@ -38,20 +55,20 @@ if __name__ == "__main__":
     clf.fit(X_train, y_train)
     y_pred_knn = clf.predict(X_test)
 
-    print("ID3")
-    print("score: ", accuracy_score(y_test, y_pred))
+    print("Decision Tree")
+    print("accuracy score: ", accuracy_score(y_test, y_pred))
     print("recall score: ", recall_score(y_test, y_pred))
-    print("accuracy score: ", precision_score(y_test, y_pred))
+    print("precision score: ", precision_score(y_test, y_pred))
 
     print("SVM")
-    print("score: ", accuracy_score(y_test, y_pred_svm))
+    print("accuracy score: ", accuracy_score(y_test, y_pred_svm))
     print("recall score: ", recall_score(y_test, y_pred_svm))
-    print("accuracy score: ", precision_score(y_test, y_pred_svm))
+    print("precision score: ", precision_score(y_test, y_pred_svm))
 
     print("KNN with N=3")
-    print("score: ", accuracy_score(y_test, y_pred_knn))
+    print("accuracy score: ", accuracy_score(y_test, y_pred_knn))
     print("recall score: ", recall_score(y_test, y_pred_knn))
-    print("accuracy score: ", precision_score(y_test, y_pred_knn))
+    print("precision score: ", precision_score(y_test, y_pred_knn))
 
-    res = pd.DataFrame({'true label': y_test.data, 'pred': y_pred})
-    res.to_csv('res.csv')
+   # # res = pd.DataFrame({'true label': y_test.data, 'pred': y_pred})
+   # # res.to_csv('res.csv')
