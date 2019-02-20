e7f0a54c35306705563fc8a95713861bc03ece23
Max Kolchinsky <max.kolchinsky@gmail.com>
2019-02-20 23:09:04 +0200

    added go term name

--- Project/code/go_term_relevance.py
+++ Project/code/go_term_relevance.py
@@ -11,16 +11,18 @@ import operator
 if __name__ == "__main__":
     df = pd.read_csv('data_labeled_no_mean.csv')
     apoptosis_related = df['apoptosis_related']
+    go_term_names = df['GO:Process']
     go_terms = df['GO:Process ID']
     df = pd.read_csv('terms.csv')
     apoptosis_related_terms = df['id']
-    go_term_counter = {}
-    for terms,is_related in zip(go_terms, apoptosis_related):
+    go_term_counter, term_map = {}, {}
+    for terms, term_names,is_related in zip(go_terms, go_term_names, apoptosis_related):
         if is_related is False:
             continue
-        for term in terms.split('///'):
+        for term, term_name in zip(terms.split('///'), term_names.split('///')):
             if term not in list(apoptosis_related_terms):
                 continue
+            term_map[term] = term_name
             go_term_counter[term] = 1 if term not in go_term_counter else go_term_counter[term] + 1
 
     sorted_terms = sorted(go_term_counter.items(), key=operator.itemgetter(1), reverse=True)
@@ -28,8 +30,9 @@ if __name__ == "__main__":
     plt.title('Go term relevance')
     plt.xlabel('Go terms')
     plt.ylabel('Count')
-    plt.plot(x, y)
-    plt.show()
+    # plt.plot(x, y)
+    # plt.show()
 
     res = pd.DataFrame(sorted_terms, columns=["go_term", "count"])
+    res['term_name'] = [term_map[term[0]] for term in sorted_terms]
     res.to_csv('go_term_relevance.csv')
