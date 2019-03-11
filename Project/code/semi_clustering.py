b233cd267c82c751e94aa29c304b803dd5117881
Max Kolchinsky <max.kolchinsky@gmail.com>
2019-03-11 13:48:22 +0200

    graphs for unsupervised score as a function of cluster num

--- Project/code/semi_clustering.py
+++ Project/code/semi_clustering.py
@@ -8,19 +8,32 @@ import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 from collections import Counter, defaultdict
 import operator
+from sklearn import metrics
 
 samples=['GSM1338298', 'GSM1338302', 'GSM1338306', 'GSM1338297', 'GSM1338301', 'GSM1338305', 'GSM1338296', 'GSM1338300',
          'GSM1338304', 'GSM1338295', 'GSM1338299', 'GSM1338303']
 
-df = pd.read_csv('train_mean.csv')
+df = pd.read_csv('data_filtered.csv')
 gene_exp = df[samples]
 
-model = KMeans(n_clusters=5)
-clusters = model.fit_predict(gene_exp)
+res = []
+for i in range(2, 8):
+    model = KMeans(n_clusters=i)
+    clusters = model.fit_predict(gene_exp)
+    # res.append(metrics.silhouette_score(gene_exp, clusters))
+    # res.append(metrics.normalized_mutual_info_score(df['apoptosis_related'], clusters))
+    # res.append(metrics.homogeneity_score(df['apoptosis_related'], clusters))
+    # res.append(metrics.fowlkes_mallows_score(df['apoptosis_related'], clusters))
+    # res.append(metrics.completeness_score(df['apoptosis_related'], clusters))
+    # res.append(metrics.davies_bouldin_score(gene_exp, clusters))
+    res.append(metrics.calinski_harabaz_score(gene_exp, clusters))
 
-test_df = pd.read_csv('test_mean.csv')
-test_gene_exp = test_df[samples]
-pred = model.predict(test_gene_exp)
-pred_dict = Counter(pred)
-sorted_pred_dict = sorted(pred_dict.items(), key=operator.itemgetter(1), reverse=True)
-print(sorted_pred_dict)
+plt.plot(range(2, 8), res)
+plt.show()
+
+# test_df = pd.read_csv('test_mean.csv')
+# test_gene_exp = test_df[samples]
+# pred = model.predict(test_gene_exp)
+# pred_dict = Counter(pred)
+# sorted_pred_dict = sorted(pred_dict.items(), key=operator.itemgetter(1), reverse=True)
+# print(sorted_pred_dict)
