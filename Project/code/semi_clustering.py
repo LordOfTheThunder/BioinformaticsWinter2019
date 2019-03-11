7932c70354464ea021b6d19caf444b4917396f65
Max Kolchinsky <max.kolchinsky@gmail.com>
2019-03-11 15:29:45 +0200

    Updated code for generating graphs for metrics and showing accuracy

--- Project/code/semi_clustering.py
+++ Project/code/semi_clustering.py
@@ -16,24 +16,45 @@ samples=['GSM1338298', 'GSM1338302', 'GSM1338306', 'GSM1338297', 'GSM1338301', '
 df = pd.read_csv('data_filtered.csv')
 gene_exp = df[samples]
 
-res = []
-for i in range(2, 8):
+res_kmeans = []
+res_hierarchical = []
+cluster_range = range(2, 15)
+for i in cluster_range:
     model = KMeans(n_clusters=i)
     clusters = model.fit_predict(gene_exp)
-    # res.append(metrics.silhouette_score(gene_exp, clusters))
+    hier_cluster = AgglomerativeClustering(n_clusters=i)
+    hier_clusters = hier_cluster.fit_predict(gene_exp)
+    res_kmeans.append(metrics.silhouette_score(gene_exp, clusters))
+    res_hierarchical.append(metrics.silhouette_score(gene_exp, hier_clusters))
+
+    # Other metrics which we decided not to use for now
     # res.append(metrics.normalized_mutual_info_score(df['apoptosis_related'], clusters))
     # res.append(metrics.homogeneity_score(df['apoptosis_related'], clusters))
     # res.append(metrics.fowlkes_mallows_score(df['apoptosis_related'], clusters))
-    # res.append(metrics.completeness_score(df['apoptosis_related'], clusters))
-    # res.append(metrics.davies_bouldin_score(gene_exp, clusters))
-    res.append(metrics.calinski_harabaz_score(gene_exp, clusters))
+    # res_kmeans.append(metrics.completeness_score(df['apoptosis_related'], clusters))
+    # res_hierarchical.append(metrics.completeness_score(df['apoptosis_related'], hier_clusters))
+    # res_kmeans.append(metrics.davies_bouldin_score(gene_exp, clusters))
+    # res_hierarchical.append(metrics.davies_bouldin_score(gene_exp, hier_clusters))
+    # res_kmeans.append(metrics.calinski_harabaz_score(gene_exp, clusters))
+    # res_hierarchical.append(metrics.calinski_harabaz_score(gene_exp, hier_clusters))
 
-plt.plot(range(2, 8), res)
+plt.plot(cluster_range, res_kmeans, 'r')
+plt.plot(cluster_range, res_hierarchical, 'g')
 plt.show()
 
-# test_df = pd.read_csv('test_mean.csv')
-# test_gene_exp = test_df[samples]
-# pred = model.predict(test_gene_exp)
-# pred_dict = Counter(pred)
-# sorted_pred_dict = sorted(pred_dict.items(), key=operator.itemgetter(1), reverse=True)
-# print(sorted_pred_dict)
+df = pd.read_csv('train_mean.csv')
+gene_exp = df[samples]
+model = KMeans(n_clusters=3)
+model.fit_predict(gene_exp)
+test_df = pd.read_csv('test_mean.csv')
+test_gene_exp = test_df[samples]
+pred = model.predict(test_gene_exp)
+pred_dict = Counter(pred)
+sorted_pred_dict = sorted(pred_dict.items(), key=operator.itemgetter(1), reverse=True)
+print(sorted_pred_dict)
+
+filtered_gene_exp = df[df['label'] == True]
+predict_true = list(model.predict(filtered_gene_exp[samples]))
+count_list = [predict_true.count(i) for i in range(model.n_clusters)]
+accuracy = max(count_list) / len(predict_true)
+print("KMeans accuracy: ", accuracy)
